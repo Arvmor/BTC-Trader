e3815c3b2dc79abf22d3775ae6e0180e3194f2e5
Arvmor <arvin.morawej@gmail.com>
2020-06-05 17:53:30 +0430

    now it supports MACD&TSI

--- USDTdatasetMaker.py
+++ USDTdatasetMaker.py
@@ -7,18 +7,41 @@ import pyautogui
 # variables
 rsival = []
 macdval = []
+tsival = []
 usdtval = []
+
+
+def writef(fpath, vname):
+    with open(fpath, "+w") as fhandle:
+        for d in vname:
+            fhandle.write("%s\n" % d)
 
 
 def MACD():
     # adding RSI chart to the Trading View
     time.sleep(2)
-    driver.find_element(
-        By.XPATH, '//*[@id="header-toolbar-indicators"]').click()
+    driver.find_element(By.XPATH, '//*[@id="header-toolbar-indicators"]').click()
     time.sleep(1)
     driver.find_element(
         By.XPATH, "/html/body/div[4]/div/div/div[2]/div[2]/div[1]/input"
     ).send_keys("MACD")
+    time.sleep(1)
+    driver.find_element(
+        By.XPATH, "/html/body/div[4]/div/div/div[2]/div[2]/div[2]/div[1]/div/div/div[1]"
+    ).click()
+    time.sleep(1)
+    driver.find_element(By.XPATH, "/html/body/div[4]/div/div/div[3]").click()
+    time.sleep(1)
+
+
+def TSI():
+    # adding RSI chart to the Trading View
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="header-toolbar-indicators"]').click()
+    time.sleep(1)
+    driver.find_element(
+        By.XPATH, "/html/body/div[4]/div/div/div[2]/div[2]/div[1]/input"
+    ).send_keys("TSI")
     time.sleep(1)
     driver.find_element(
         By.XPATH, "/html/body/div[4]/div/div/div[2]/div[2]/div[2]/div[1]/div/div/div[1]"
@@ -37,8 +60,7 @@ def RSI():
         )
     )
     time.sleep(2)
-    driver.find_element(
-        By.XPATH, '//*[@id="header-toolbar-indicators"]').click()
+    driver.find_element(By.XPATH, '//*[@id="header-toolbar-indicators"]').click()
     time.sleep(1)
     driver.find_element(
         By.XPATH, "/html/body/div[4]/div/div/div[2]/div[2]/div[1]/input"
@@ -68,27 +90,52 @@ def checkPriceValue():
         By.XPATH,
         "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/table/tr[1]/td[2]/div/div[3]/div/div/span[4]/span[2]",
     ).text.encode("utf-8")
-    Pricevalue = int((Pricevalue.decode("utf-8"))[1:5])*10
+    Pricevalue = int((Pricevalue.decode("utf-8"))[1:5]) * 10
     return Pricevalue
 
 
-# def checkMACDValue():
-#     # getting RSI current value
-#     RSIvalue = driver.find_element(
-#         By.XPATH,
-#         "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/table/tr[5]/td[2]/div/div[3]/div/div/span[1]/span",
-#     ).text.encode("utf-8")
-#     RSIvalue = float((RSIvalue.decode("utf-8"))[1:5])
-#     print(RSIvalue)
-#     return RSIvalue
+def checkMACDValue():
+    # getting RSI current value
+    RSIvalue = driver.find_element(
+        By.XPATH,
+        "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/table/tr[5]/td[2]/div/div[3]/div/div/span[3]/span",
+    ).text.encode("utf-8")
+    if RSIvalue.decode("utf-8")[1] == "\u2212":
+        RSIvalue = int((RSIvalue.decode("utf-8"))[2:-6]) * -1
+    else:
+        RSIvalue = int((RSIvalue.decode("utf-8"))[1:-6])
+    RSI2value = driver.find_element(
+        By.XPATH,
+        "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/table/tr[5]/td[2]/div/div[3]/div/div/span[2]/span",
+    ).text.encode("utf-8")
+    if RSI2value.decode("utf-8")[1] == "\u2212":
+        RSI2value = int((RSI2value.decode("utf-8"))[2:-6]) * -1
+    else:
+        RSI2value = int((RSI2value.decode("utf-8"))[1:-6])
+    return abs(RSIvalue - RSI2value)
+
+
+def checkTSIValue():
+    # getting RSI current value
+    RSIvalue = driver.find_element(
+        By.XPATH,
+        "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/table/tr[7]/td[2]/div/div[3]/div/div/span[1]/span",
+    ).text.encode("utf-8")
+    if RSIvalue.decode("utf-8")[1] == "\u2212":
+        RSIvalue = float((RSIvalue.decode("utf-8"))[2:-4]) * -1
+    else:
+        RSIvalue = float((RSIvalue.decode("utf-8"))[1:-4])
+    return RSIvalue
 
 
 def getDatas(days):
     for day in range(days):
-        pyautogui.press('left')
-        time.sleep(0.3)
+        pyautogui.press("left")
+        time.sleep(0.7)
         usdtval.append(checkPriceValue())
         rsival.append(checkRSIValue())
+        macdval.append(checkMACDValue())
+        tsival.append(checkTSIValue())
 
 
 # Driver settings
@@ -101,13 +148,10 @@ time.sleep(15)
 # run functions
 RSI()
 MACD()
+TSI()
 time.sleep(5)
-getDatas(2000)
-
-with open('datasetRSIval.txt', '+w') as filehandle:
-    for rval in rsival:
-        filehandle.write('%s\n' % rval)
-
-with open('datasetPrice.txt', '+w') as fhandle:
-    for usv in usdtval:
-        fhandle.write('%s\n' % usv)
+getDatas(3750)
+writef("datasetRSIval.txt", rsival)
+writef("datasetPrice.txt", usdtval)
+writef("datasetMACD.txt", macdval)
+writef("datasetTSI.txt", tsival)

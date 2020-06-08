cfe980b71644529de1d77c8abe44078a9704e6d9
Arvin Morawej <arvin.morawej@gmail.com>
2020-06-08 13:28:32 +0430

    Update TestOnDatasets.py

--- Test strategy/TestOnDatasets.py
+++ Test strategy/TestOnDatasets.py
@@ -5,21 +5,24 @@ price = []
 rsi = []
 tsi = []
 macd = []
+bb = []
 highestBalance = []
 bestValue = []
 bestValue2 = []
 bestValue3 = []
 bestValue4 = []
 bestValue5 = []
+bestValue6 = []
+bestValue7 = []
+bestValue8 = []
 
 
 def load(filename, indicator):
-    with open(
-        "C:/Users/123/Documents/GitHub/USD-Trader - code/datasets/" + filename, "r"
-    ) as fhandle:
-        for l in fhandle:
-            cPlace = l[:-1]
-            indicator.append(cPlace)
+    f = open("../datasets/"+filename, "r")
+    for l in f:
+        cPlace = l[:-1]
+        indicator.append(cPlace)
+    f.close()
 
 
 # open file and read the content in a list
@@ -27,9 +30,9 @@ load("datasetPrice.txt", price)
 load("datasetRSIval.txt", rsi)
 load("datasetTSI.txt", tsi)
 load("datasetMACD.txt", macd)
-
+load("datasetBB.txt", bb)
 # testing our strategy
-for x in range(10000000):
+for x in range(7000000):
     rialPocket = 100000
     usdtPocket = 0
     i = 0
@@ -39,15 +42,24 @@ for x in range(10000000):
     # random values for AI
     bv = random.choice(range(10)) / -10
     sv = random.choice(range(10)) / 10
-    b2v = random.choice(range(30, 50)) + (random.choice(range(10)) / 10)
-    s2v = random.choice(range(50, 65)) + (random.choice(range(10)) / 10)
-    macdv = random.choice(range(0, int(max(macd))+1))
+    b2v = random.choice(range(30, 51))
+    s2v = random.choice(range(50, 71))
+    bmacdv = random.choice(range(0, 10))
+    smacdv = random.choice(range(0, 10))
+    bbbv = random.choice(range(0, 10))
+    sbbv = random.choice(range(0, 10))
     # test on our data
-    while i != 1999:
+    while i != 3809:
         while True:
-            if bought == False and i != 1999:
-                if float(rsi[-i]) <= b2v and float(tsi[-i]) <= bv and int(macd[-i]) >= macdv:
-                    usdtPocket = (rialPocket / int(price[-i])) * 0.9965
+            if bought == False and i != 3809:
+                if (
+                    float(rsi[-i]) <= b2v
+                    and float(tsi[-i]) <= bv
+                    and int(macd[-i])/10 >= bmacdv
+                    and float(int(bb[-i])/100) >= bbbv
+                ):
+                    usdtPocket = (
+                        rialPocket / int(price[-i])) * 0.9965
                     rialPocket -= rialPocket
                     bought = True
                     tradeMade += 1
@@ -57,9 +69,15 @@ for x in range(10000000):
                 break
 
         while True:
-            if sold == False and i != 1999:
-                if float(rsi[-i]) >= s2v and float(tsi[-i]) >= sv and int(macd[-i]) >= macdv:
-                    rialPocket = (usdtPocket * int(price[-i])) * 0.9965
+            if sold == False and i != 3809:
+                if (
+                    float(rsi[-i]) >= s2v
+                    and float(tsi[-i]) >= sv
+                    and int(macd[-i])/10 >= smacdv
+                    and float(int(bb[-i])/100) >= sbbv
+                ):
+                    rialPocket = (
+                        usdtPocket * int(price[-i])) * 0.9965
                     usdtPocket -= usdtPocket
                     sold = True
                     tradeMade += 1
@@ -68,16 +86,20 @@ for x in range(10000000):
                 sold = False
                 break
     if rialPocket == 0:
-        rialPocket = usdtPocket * 17600
-    highestBalance.append(rialPocket)
-    bestValue.append(bv)
-    bestValue2.append(sv)
-    bestValue3.append(b2v)
-    bestValue4.append(s2v)
-    bestValue5.append(macdv)
+        rialPocket = usdtPocket * 17800
+    if rialPocket >= 101000:
+        highestBalance.append(rialPocket)
+        bestValue.append(bv)
+        bestValue2.append(sv)
+        bestValue3.append(b2v)
+        bestValue4.append(s2v)
+        bestValue5.append(bmacdv)
+        bestValue6.append(smacdv)
+        bestValue7.append(bbbv)
+        bestValue8.append(sbbv)
 
 # finding best result
 maxi = highestBalance.index(max(highestBalance))
 print(
-    f"{usdtPocket} {highestBalance[maxi]}, TSI's: {bestValue[maxi]} {bestValue2[maxi]}, RSI's: {bestValue3[maxi]} {bestValue4[maxi]}, MACD: {bestValue5[maxi]}, {tradeMade} Trade were made !"
+    f"{usdtPocket} {highestBalance[maxi]}, RSI's: {bestValue3[maxi]} {bestValue4[maxi]}, TSI's: {bestValue[maxi]} {bestValue2[maxi]}, MACD: {bestValue5[maxi]} {bestValue6[maxi]}, BB: {bestValue7[maxi]} {bestValue8[maxi]} !"
 )

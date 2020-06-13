e858dca5f2fa92a712685dc786cfd87983d7e45c
Arvmor <Arvin.morawej@gmail.com>
2020-06-13 18:18:07 +0430

    Update TraceTrades.py

--- Test strategy/TraceTrades.py
+++ Test strategy/TraceTrades.py
@@ -9,7 +9,7 @@ bb = []
 
 
 def load(filename, indicator):
-    f = open(filename, "r")
+    f = open("../datasets/"+filename, "r")
     for l in f:
         cPlace = l[:-1]
         indicator.append(cPlace)
@@ -20,7 +20,7 @@ def load(filename, indicator):
 load("datasetPrice.txt", price)
 load("datasetRSIval.txt", rsi)
 load("datasetTSI.txt", tsi)
-load("datasetMACD2.txt", macd)
+load("datasetMACD.txt", macd)
 load("datasetBB.txt", bb)
 
 # testing our strategy
@@ -36,13 +36,13 @@ while i != 3809:
             if (
                 float(rsi[-i]) <= 50
                 and float(tsi[-i]) <= -0.0
-                and int(macd[-i]) >= 3497
-                and float(int(bb[-i])/100) >= 10
+                and float(macd[-i])/10 >= 3.5
+                and float(int(bb[-i])/100) >= 3
             ):
                 usdtPocket = (rialPocket / int(price[-i])) * 0.9965
                 rialPocket -= rialPocket
                 print(
-                    f"Bought ! usdt={int(price[-i])}, RSI={rsi[-i]}, TSI={tsi[-i]}, MACD={macd[-i]}, BB={bb[-i]}, {usdtPocket}, {i}"
+                    f"B ! usdt={int(price[-i])}, RSI={rsi[-i]}, TSI={tsi[-i]}, MACD={float(macd[-i])/10}, BB={float(bb[-i])/100}, {usdtPocket}, {i}"
                 )
                 bought = True
             i += 1
@@ -53,15 +53,15 @@ while i != 3809:
     while True:
         if sold == False and i != 3809:
             if (
-                float(rsi[-i]) >= 63
-                and float(tsi[-i]) >= 0.1
-                and int(macd[-i]) >= 3497
-                and float(int(bb[-i])/100) >= 10
+                float(rsi[-i]) >= 65
+                and float(tsi[-i]) >= 0.7
+                and float(macd[-i])/10 >= 2.2
+                and float(int(bb[-i])/100) >= 7
             ):
                 rialPocket = (usdtPocket * int(price[-i])) * 0.9965
                 usdtPocket -= usdtPocket
                 print(
-                    f"Sold ! usdt={int(price[-i])}, RSI={rsi[-i]}, TSI={tsi[-i]}, MACD={macd[-i]}, BB={bb[-i]}, {rialPocket}, {i}"
+                    f"S ! usdt={int(price[-i])}, RSI={rsi[-i]}, TSI={tsi[-i]}, MACD={float(macd[-i])/10}, BB={float(bb[-i])/100}, {rialPocket}, {i}"
                 )
                 sold = True
             i += 1
@@ -70,6 +70,6 @@ while i != 3809:
             break
 
 if rialPocket == 0:
-    rialPocket = usdtPocket
+    rialPocket = usdtPocket * 17800
 # finding best result
 print(f"balance: {rialPocket}")

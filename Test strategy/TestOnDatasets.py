2e000cded536468701b187b4d348cc1882fb26f1
Arvmor <Arvin.morawej@gmail.com>
2020-06-14 14:26:37 +0430

    fix

--- Test strategy/TestOnDatasets.py
+++ Test strategy/TestOnDatasets.py
@@ -15,6 +15,16 @@ bestValue5 = []
 bestValue6 = []
 bestValue7 = []
 bestValue8 = []
+bestValue9 = []
+bestValue10 = []
+bestValue11 = []
+bestValue12 = []
+bestValue13 = []
+bestValue14 = []
+bestValue15 = []
+bestValue16 = []
+bestValue17 = []
+bestValue18 = []
 
 
 def load(filename, indicator):
@@ -32,7 +42,7 @@ load("datasetTSI.txt", tsi)
 load("datasetMACD.txt", macd)
 load("datasetBB.txt", bb)
 # testing our strategy
-for x in range(7000000):
+for x in range(15000):
     rialPocket = 100000
     usdtPocket = 0
     i = 0
@@ -42,13 +52,21 @@ for x in range(7000000):
     bought = False
     # random values for AI
     b1v = random.choice(range(10)) / -10
+    b1v2 = random.choice(range(6)) / 10
     s1v = random.choice(range(10)) / 10
+    s1v2 = random.choice(range(6)) / 10
     b2v = random.choice(range(30, 51))
+    b2v2 = random.choice(range(10))
     s2v = random.choice(range(50, 71))
-    b3v = random.choice(range(0, 10))
-    s3v = random.choice(range(0, 10))
-    b4v = random.choice(range(0, 10))
-    s4v = random.choice(range(0, 10))
+    s2v2 = random.choice(range(10))
+    b3v = random.choice(range(10))
+    b3v2 = random.choice(range(10))
+    s3v = random.choice(range(10))
+    s3v2 = random.choice(range(10))
+    b4v = random.choice(range(10))
+    b4v2 = random.choice(range(10))
+    s4v = random.choice(range(10))
+    s4v2 = random.choice(range(10))
     b5v = random.choice(range(2, 5)) + (random.choice(range(0, 10, 5))/10)
     s5v = random.choice(range(2, 5)) + (random.choice(range(0, 10, 5))/10)
     # test on our data which is 3810 lines
@@ -61,22 +79,22 @@ for x in range(7000000):
                     if True:
                         if float(tsi[-i]) <= b1v:
                             confident += 1
-                        elif float(tsi[-i]) <= b1v + 0.3:
+                        elif float(tsi[-i]) <= b1v + b1v2:
                             confident += 0.5
                     if True:
                         if float(rsi[-i]) <= b2v:
                             confident += 1
-                        elif float(rsi[-i]) <= b2v + 5:
+                        elif float(rsi[-i]) <= b2v + b2v2:
                             confident += 0.5
                     if True:
                         if int(macd[-i])/10 >= b3v:
                             confident += 1
-                        elif int(macd[-i])/10 >= b3v + 5:
+                        elif int(macd[-i])/10 >= b3v + b3v2:
                             confident += 0.5
                     if True:
                         if float(int(bb[-i])/100) >= b4v:
                             confident += 1
-                        elif float(int(bb[-i])/100) >= b4v + 5:
+                        elif float(int(bb[-i])/100) >= b4v + b4v2:
                             confident += 0.5
                 # looking for good situation to buy
                 if (confident >= b5v):
@@ -99,22 +117,22 @@ for x in range(7000000):
                     if True:
                         if float(tsi[-i]) >= s1v:
                             confident += 1
-                        elif float(tsi[-i]) >= s1v - 0.3:
+                        elif float(tsi[-i]) >= s1v - s1v2:
                             confident += 0.5
                     if True:
                         if float(rsi[-i]) >= s2v:
                             confident += 1
-                        elif float(rsi[-i]) >= s2v - 5:
+                        elif float(rsi[-i]) >= s2v - s2v2:
                             confident += 0.5
                     if True:
                         if int(macd[-i])/10 >= s3v:
                             confident += 1
-                        elif int(macd[-i])/10 >= s3v - 5:
+                        elif int(macd[-i])/10 >= s3v - s3v2:
                             confident += 0.5
                     if True:
                         if float(int(bb[-i])/100) >= s4v:
                             confident += 1
-                        elif float(int(bb[-i])/100) >= s4v - 5:
+                        elif float(int(bb[-i])/100) >= s4v - s4v2:
                             confident += 0.5
                 # looking for good situation to sell
                 if (confident >= s5v):
@@ -130,7 +148,8 @@ for x in range(7000000):
                 break
     if usdtPocket == 0:
         usdtPocket = rialPocket / 18400
-    if usdtPocket >= 6:
+    if usdtPocket >= 9:
+        #results
         highestBalance.append(usdtPocket)
         bestValue.append(b1v)
         bestValue2.append(s1v)
@@ -140,9 +159,22 @@ for x in range(7000000):
         bestValue6.append(s3v)
         bestValue7.append(b4v)
         bestValue8.append(s4v)
+        bestValue9.append(b1v2)
+        bestValue10.append(s1v2)
+        bestValue11.append(b2v2)
+        bestValue12.append(s2v2)
+        bestValue13.append(b3v2)
+        bestValue14.append(s3v2)
+        bestValue15.append(b4v2)
+        bestValue16.append(s4v2)
+        bestValue17.append(b5v)
+        bestValue18.append(s5v)
 
 # finding best result
 maxi = highestBalance.index(max(highestBalance))
 print(
-    f"{usdtPocket} {highestBalance[maxi]}, RSI's: {bestValue3[maxi]} {bestValue4[maxi]}, TSI's: {bestValue[maxi]} {bestValue2[maxi]}, MACD: {bestValue5[maxi]} {bestValue6[maxi]}, BB: {bestValue7[maxi]} {bestValue8[maxi]} !"
+    f"{highestBalance[maxi]}, RSI's: {bestValue3[maxi]} {bestValue4[maxi]}, TSI's: {bestValue[maxi]} {bestValue2[maxi]}, MACD: {bestValue5[maxi]} {bestValue6[maxi]}, BB: {bestValue7[maxi]} {bestValue8[maxi]} !"
+)
+print(
+    f"TSIc: {bestValue9[maxi]} {bestValue10[maxi]}, RSIc: {bestValue11[maxi]} {bestValue12[maxi]}, MACDc {bestValue13[maxi]} {bestValue14[maxi]}, BBc: {bestValue15[maxi]} {bestValue16[maxi]}, Confidenc: {bestValue17[maxi]} {bestValue18[maxi]}"
 )

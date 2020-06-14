d3543aa82636cc3f0c15d6da5f2b32ef5d048a33
Arvmor <Arvin.morawej@gmail.com>
2020-06-14 13:54:54 +0430

    adding confidence

--- Test strategy/TestOnDatasets.py
+++ Test strategy/TestOnDatasets.py
@@ -37,27 +37,48 @@ for x in range(7000000):
     usdtPocket = 0
     i = 0
     tradeMade = 0
+    confident = 0
     sold = False
     bought = False
     # random values for AI
-    bv = random.choice(range(10)) / -10
-    sv = random.choice(range(10)) / 10
+    b1v = random.choice(range(10)) / -10
+    s1v = random.choice(range(10)) / 10
     b2v = random.choice(range(30, 51))
     s2v = random.choice(range(50, 71))
-    bmacdv = random.choice(range(0, 10))
-    smacdv = random.choice(range(0, 10))
-    bbbv = random.choice(range(0, 10))
-    sbbv = random.choice(range(0, 10))
-    # test on our data
+    b3v = random.choice(range(0, 10))
+    s3v = random.choice(range(0, 10))
+    b4v = random.choice(range(0, 10))
+    s4v = random.choice(range(0, 10))
+    b5v = random.choice(range(2, 5)) + (random.choice(range(0, 10, 5))/10)
+    s5v = random.choice(range(2, 5)) + (random.choice(range(0, 10, 5))/10)
+    # test on our data which is 3810 lines
     while i != 3809:
         while True:
             if bought == False and i != 3809:
-                if (
-                    float(rsi[-i]) <= b2v
-                    and float(tsi[-i]) <= bv
-                    and int(macd[-i])/10 >= bmacdv
-                    and float(int(bb[-i])/100) >= bbbv
-                ):
+                # calculating confidence
+                if True:
+                    if True:
+                        if float(tsi[-i]) <= b1v:
+                            confident += 1
+                        elif float(tsi[-i]) <= b1v + 0.3:
+                            confident += 0.5
+                    if True:
+                        if float(rsi[-i]) <= b2v:
+                            confident += 1
+                        elif float(rsi[-i]) <= b2v + 5:
+                            confident += 0.5
+                    if True:
+                        if int(macd[-i])/10 >= b3v:
+                            confident += 1
+                        elif int(macd[-i])/10 >= b3v + 5:
+                            confident += 0.5
+                    if True:
+                        if float(int(bb[-i])/100) >= b4v:
+                            confident += 1
+                        elif float(int(bb[-i])/100) >= b4v + 5:
+                            confident += 0.5
+                # looking for good situation to buy
+                if (confident >= b5v):
                     usdtPocket = (
                         rialPocket / int(price[-i])) * 0.9965
                     rialPocket -= rialPocket
@@ -70,12 +91,30 @@ for x in range(7000000):
 
         while True:
             if sold == False and i != 3809:
-                if (
-                    float(rsi[-i]) >= s2v
-                    and float(tsi[-i]) >= sv
-                    and int(macd[-i])/10 >= smacdv
-                    and float(int(bb[-i])/100) >= sbbv
-                ):
+                # calculating confidence
+                if True:
+                    if True:
+                        if float(tsi[-i]) >= s1v:
+                            confident += 1
+                        elif float(tsi[-i]) >= s1v - 0.3:
+                            confident += 0.5
+                    if True:
+                        if float(rsi[-i]) >= s2v:
+                            confident += 1
+                        elif float(rsi[-i]) >= s2v - 5:
+                            confident += 0.5
+                    if True:
+                        if int(macd[-i])/10 >= s3v:
+                            confident += 1
+                        elif int(macd[-i])/10 >= s3v - 5:
+                            confident += 0.5
+                    if True:
+                        if float(int(bb[-i])/100) >= s4v:
+                            confident += 1
+                        elif float(int(bb[-i])/100) >= s4v - 5:
+                            confident += 0.5
+                # looking for good situation to sell
+                if (confident >= s5v):
                     rialPocket = (
                         usdtPocket * int(price[-i])) * 0.9965
                     usdtPocket -= usdtPocket
@@ -89,14 +128,14 @@ for x in range(7000000):
         usdtPocket = rialPocket / 18400
     if usdtPocket >= 6:
         highestBalance.append(usdtPocket)
-        bestValue.append(bv)
-        bestValue2.append(sv)
+        bestValue.append(b1v)
+        bestValue2.append(s1v)
         bestValue3.append(b2v)
         bestValue4.append(s2v)
-        bestValue5.append(bmacdv)
-        bestValue6.append(smacdv)
-        bestValue7.append(bbbv)
-        bestValue8.append(sbbv)
+        bestValue5.append(b3v)
+        bestValue6.append(s3v)
+        bestValue7.append(b4v)
+        bestValue8.append(s4v)
 
 # finding best result
 maxi = highestBalance.index(max(highestBalance))

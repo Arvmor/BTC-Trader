63a92bf5bac5d636046d8881d4e07770a8966fec
Arvmor <Arvin.morawej@gmail.com>
2020-06-14 14:04:26 +0430

    Add confidence

--- Test strategy/TraceTrades.py
+++ Test strategy/TraceTrades.py
@@ -27,18 +27,38 @@ load("datasetBB.txt", bb)
 for x in range(1):
     rialPocket = 100000
     usdtPocket = 0
+    confident = 0
     i = 0
     sold = False
     bought = False
 while i != 3809:
     while True:
         if bought == False and i != 3809:
-            if (
-                float(rsi[-i]) <= 47
-                and float(tsi[-i]) <= -0.1
-                and float(macd[-i])/10 >= 7
-                and float(int(bb[-i])/100) >= 6
-            ):
+            confident = 0
+            # calculating confidence
+            if True:
+                if True:
+                    if float(tsi[-i]) <= -0.1:
+                        confident += 1
+                    elif float(tsi[-i]) <= -0.1 + 0.3:
+                        confident += 0.5
+                if True:
+                    if float(rsi[-i]) <= 47:
+                        confident += 1
+                    elif float(rsi[-i]) <= 47 + 5:
+                        confident += 0.5
+                if True:
+                    if int(macd[-i])/10 >= 7:
+                        confident += 1
+                    elif int(macd[-i])/10 >= 7 + 5:
+                        confident += 0.5
+                if True:
+                    if float(int(bb[-i])/100) >= 6:
+                        confident += 1
+                    elif float(int(bb[-i])/100) >= 6 + 5:
+                        confident += 0.5
+            # looking for good situation to buy
+            if (confident >= 2.5):
                 usdtPocket = (rialPocket / int(price[-i])) * 0.9965
                 rialPocket -= rialPocket
                 print(
@@ -47,17 +67,37 @@ while i != 3809:
                 bought = True
             i += 1
         else:
+            confident = 0
             bought = False
             break
 
     while True:
         if sold == False and i != 3809:
-            if (
-                float(rsi[-i]) >= 70
-                and float(tsi[-i]) >= 0.6
-                and float(macd[-i])/10 >= 8
-                and float(int(bb[-i])/100) >= 6
-            ):
+            confident = 0
+            # calculating confidence
+            if True:
+                if True:
+                    if float(tsi[-i]) >= 0.6:
+                        confident += 1
+                    elif float(tsi[-i]) >= 0.6 - 0.3:
+                        confident += 0.5
+                if True:
+                    if float(rsi[-i]) >= 70:
+                        confident += 1
+                    elif float(rsi[-i]) >= 70 - 5:
+                        confident += 0.5
+                if True:
+                    if int(macd[-i])/10 >= 8:
+                        confident += 1
+                    elif int(macd[-i])/10 >= 8 - 5:
+                        confident += 0.5
+                if True:
+                    if float(int(bb[-i])/100) >= 6:
+                        confident += 1
+                    elif float(int(bb[-i])/100) >= 6 - 5:
+                        confident += 0.5
+            # looking for good situation to sell
+            if (confident >= 2.5):
                 rialPocket = (usdtPocket * int(price[-i])) * 0.9965
                 usdtPocket -= usdtPocket
                 print(
@@ -66,6 +106,7 @@ while i != 3809:
                 sold = True
             i += 1
         else:
+            confident = 0
             sold = False
             break
 

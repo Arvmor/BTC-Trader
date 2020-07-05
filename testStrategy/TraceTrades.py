0c45f715a2245429be130d8d49769299858dd8a6
Arvmor <Arvin.morawej@gmail.com>
2020-07-05 11:04:30 +0430

    Add APPT

--- testStrategy/TraceTrades.py
+++ testStrategy/TraceTrades.py
@@ -18,6 +18,9 @@ def load(filename, indicator):
         indicator.append(cPlace)
     f.close()
 
+def Average(lst): 
+    return sum(lst) / len(lst)
+
 if argv[1] == "dataset":
     # open file and read the content in a list
     load("datasetPrice.txt", price)
@@ -78,105 +81,109 @@ if len(argv) >= 3:
     for value in range(len(testValues)):
         testValues[value] = float(testValues[value])
 # testing our strategy
-for x in range(1):
-    rialPocket = 100000
-    btcPocket = 0
-    confidence = 0
-    i = 0
-    sold = False
-    bought = False
-    while i != len(price):
-        while True:
-            if bought == False and i != len(price):
-                btcPocket -= btcPocket
-                confidence = 0
-                # calculating confidence
+totalSum = []
+profit = [100000]
+rialPocket = 100000
+btcPocket = 0
+confidence = 0
+i = 0
+sold = False
+bought = False
+while i != len(price):
+    while True:
+        if bought == False and i != len(price):
+            btcPocket -= btcPocket
+            confidence = 0
+            # calculating confidence
+            if True:
                 if True:
-                    if True:
-                        if float(tsi[-i]) <= testValues[2]:
-                            confidence += 1
-                        elif float(tsi[-i]) <= testValues[2] + testValues[10]:
-                            confidence += 0.5
-                    if volume[-i][0] == 'R':
-                        if float(float(volume[-i][1:])/10) >= testValues[8]:
-                            confidence += 1
-                        elif float(float(volume[-i][1:])/10) >= testValues[8] - testValues[18]:
-                            confidence += 0.5
-                    if True:
-                        if float(rsi[-i]) <= testValues[0]:
-                            confidence += 1
-                        elif float(rsi[-i]) <= testValues[0] + testValues[12]:
-                            confidence += 0.5
-                    if True:
-                        if float(macd[-i]) <= testValues[4]:
-                            confidence += 1
-                        elif float(macd[-i]) <= testValues[4] + testValues[14]:
-                            confidence += 0.5
-                    if True:
-                        if float(bb[-i]) >= testValues[6]:
-                            confidence += 1
-                        elif float(bb[-i]) >= testValues[6] - testValues[16]:
-                            confidence += 0.5
-                # looking for good situation to buy
-                if (confidence >= testValues[20]) and float(smiio[-i]) <= testValues[22]:
-                    btcPocket = (rialPocket / int(price[-i])) * 0.9965
-                    print(
-                        f"B !Confidence:{confidence}/{testValues[20]}, BTC={int(price[-i])}, RSI={rsi[-i]}/{testValues[0]}, TSI={tsi[-i]}/{testValues[2]}, MACD={macd[-i]}/{testValues[4]}, BB={bb[-i]}/{testValues[6]}, Volume={volume[-i]}/{testValues[8]*10}, SMIIO={smiio[-i]}/{testValues[22]}, ROC={roc[-i]}, {btcPocket}, {len(price)-i}"
-                    )
-                    bought = True
-                i += 1
-            else:
-                confidence = 0
-                bought = False
-                break
+                    if float(tsi[-i]) <= testValues[2]:
+                        confidence += 1
+                    elif float(tsi[-i]) <= testValues[2] + testValues[10]:
+                        confidence += 0.5
+                if volume[-i][0] == 'R':
+                    if float(float(volume[-i][1:])/10) >= testValues[8]:
+                        confidence += 1
+                    elif float(float(volume[-i][1:])/10) >= testValues[8] - testValues[18]:
+                        confidence += 0.5
+                if True:
+                    if float(rsi[-i]) <= testValues[0]:
+                        confidence += 1
+                    elif float(rsi[-i]) <= testValues[0] + testValues[12]:
+                        confidence += 0.5
+                if True:
+                    if float(macd[-i]) <= testValues[4]:
+                        confidence += 1
+                    elif float(macd[-i]) <= testValues[4] + testValues[14]:
+                        confidence += 0.5
+                if True:
+                    if float(bb[-i]) >= testValues[6]:
+                        confidence += 1
+                    elif float(bb[-i]) >= testValues[6] - testValues[16]:
+                        confidence += 0.5
+            # looking for good situation to buy
+            if (confidence >= testValues[20]) and float(smiio[-i]) <= testValues[22]:
+                btcPocket = (rialPocket / int(price[-i])) * 0.9965
+                print(
+                    f"B !Confidence:{confidence}/{testValues[20]}, BTC={int(price[-i])}, RSI={rsi[-i]}/{testValues[0]}, TSI={tsi[-i]}/{testValues[2]}, MACD={macd[-i]}/{testValues[4]}, BB={bb[-i]}/{testValues[6]}, Volume={volume[-i]}/{testValues[8]*10}, SMIIO={smiio[-i]}/{testValues[22]}, ROC={roc[-i]}, {btcPocket}, {len(price)-i}"
+                )
+                bought = True
+            i += 1
+        else:
+            confidence = 0
+            bought = False
+            break
 
-        while True:
-            if sold == False and i != len(price):
-                rialPocket -= rialPocket
-                confidence = 0
-                # calculating confidence
+    while True:
+        if sold == False and i != len(price):
+            rialPocket -= rialPocket
+            confidence = 0
+            # calculating confidence
+            if True:
                 if True:
-                    if True:
-                        if float(tsi[-i]) >= testValues[3]:
-                            confidence += 1
-                        elif float(tsi[-i]) >= testValues[3] - testValues[11]:
-                            confidence += 0.5
-                    if volume[-i][0] == 'G':
-                        if float(float(volume[-i][1:])/10) >= testValues[9]:
-                            confidence += 1
-                        elif float(float(volume[-i][1:])/10) >= testValues[9] - testValues[19]:
-                            confidence += 0.5
-                    if True:
-                        if float(rsi[-i]) >= testValues[1]:
-                            confidence += 1
-                        elif float(rsi[-i]) >= testValues[1] - testValues[13]:
-                            confidence += 0.5
-                    if True:
-                        if float(macd[-i]) >= testValues[5]:
-                            confidence += 1
-                        elif float(macd[-i]) >= testValues[5] - testValues[15]:
-                            confidence += 0.5
-                    if True:
-                        if float(bb[-i]) >= testValues[7]:
-                            confidence += 1
-                        elif float(bb[-i]) >= testValues[7] - testValues[17]:
-                            confidence += 0.5
-                # looking for good situation to sell
-                if (confidence >= testValues[21]) and float(smiio[-i]) >= testValues[23]:
-                    rialPocket = (btcPocket * int(price[-i])) * 0.9965
-                    print(
-                        f"S !Confidence:{confidence}/{testValues[21]}, BTC={int(price[-i])}, RSI={rsi[-i]}/{testValues[1]}, TSI={tsi[-i]}/{testValues[3]}, MACD={macd[-i]}/{testValues[5]}, BB={bb[-i]}/{testValues[7]}, Volume={volume[-i]}/{testValues[9]*10}, SMIIO={smiio[-i]}/{testValues[23]}, ROC={roc[-i]}, {rialPocket}, {len(price)-i}"
-                    )
-                    sold = True
-                i += 1
-            else:
-                confidence = 0
-                sold = False
-                break
+                    if float(tsi[-i]) >= testValues[3]:
+                        confidence += 1
+                    elif float(tsi[-i]) >= testValues[3] - testValues[11]:
+                        confidence += 0.5
+                if volume[-i][0] == 'G':
+                    if float(float(volume[-i][1:])/10) >= testValues[9]:
+                        confidence += 1
+                    elif float(float(volume[-i][1:])/10) >= testValues[9] - testValues[19]:
+                        confidence += 0.5
+                if True:
+                    if float(rsi[-i]) >= testValues[1]:
+                        confidence += 1
+                    elif float(rsi[-i]) >= testValues[1] - testValues[13]:
+                        confidence += 0.5
+                if True:
+                    if float(macd[-i]) >= testValues[5]:
+                        confidence += 1
+                    elif float(macd[-i]) >= testValues[5] - testValues[15]:
+                        confidence += 0.5
+                if True:
+                    if float(bb[-i]) >= testValues[7]:
+                        confidence += 1
+                    elif float(bb[-i]) >= testValues[7] - testValues[17]:
+                        confidence += 0.5
+            # looking for good situation to sell
+            if (confidence >= testValues[21]) and float(smiio[-i]) >= testValues[23]:
+                rialPocket = (btcPocket * int(price[-i])) * 0.9965
+                profit.append(rialPocket)
+                print(
+                    f"S !Confidence:{confidence}/{testValues[21]}, BTC={int(price[-i])}, RSI={rsi[-i]}/{testValues[1]}, TSI={tsi[-i]}/{testValues[3]}, MACD={macd[-i]}/{testValues[5]}, BB={bb[-i]}/{testValues[7]}, Volume={volume[-i]}/{testValues[9]*10}, SMIIO={smiio[-i]}/{testValues[23]}, ROC={roc[-i]}, {rialPocket}, {len(price)-i}"
+                )
+                sold = True
+            i += 1
+        else:
+            confidence = 0
+            sold = False
+            break
 
 if rialPocket == 0:
     rialPocket = btcPocket * 190000000
 if btcPocket == 0:
     btcPocket = rialPocket / 190000000
-# finding best result
-print(f"balance: {rialPocket}, {btcPocket}")
+# average profit per trade
+for value in range(len(profit)-1):
+    totalSum.append( profit[value+1] - profit[value])
+print(f"balance: {rialPocket}, {btcPocket}, Average Profit: {int(Average(totalSum))/1000}%")

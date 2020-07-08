4a573985b9cc3ef5d90456db04afc04942d85ff2
Arvmor <Arvin.morawej@gmail.com>
2020-07-08 15:48:58 +0430

    Add clock

--- webPanel/index.php
+++ webPanel/index.php
@@ -9,6 +9,29 @@
 <body>
     <div class="container-contact100">
         <div class="wrap-contact100">
+            <div id="MyClockDisplay" class="contact100-form-title" onload="showTime()" style="margin: -80px 0px 0px 0px;"></div>
+            <script>function showTime() {
+                    var date = new Date();
+                    var h = date.getHours(); // 0 - 23
+                    var m = date.getMinutes(); // 0 - 59
+                    var s = date.getSeconds(); // 0 - 59
+                    var session = "AM";
+                    if (h == 0) {
+                        h = 12;
+                    }
+                    if (h > 12) {
+                        h = h - 12;
+                        session = "PM";
+                    }
+                    h = (h < 10) ? "0" + h : h;
+                    m = (m < 10) ? "0" + m : m;
+                    s = (s < 10) ? "0" + s : s;
+                    var time = h + ":" + m + ":" + s + " " + session;
+                    document.getElementById("MyClockDisplay").innerText = time;
+                    document.getElementById("MyClockDisplay").textContent = time;
+                    setTimeout(showTime, 1000);
+                }
+                showTime();</script>
             <a href="#" onClick="window.location.reload();">Refresh</a>
             <script src="./btnClick.js"></script>
             <span class="contact100-form-title">
